1571a8e3605586f4e4f380b8b610de946582aea0
Kalpit Mody <kalpitmody@gmail.com>
2023-01-19 00:47:13 -0600

    modifying details page

--- src/app/details-page/details-page.page.ts
+++ src/app/details-page/details-page.page.ts
@@ -1,20 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { Chart } from 'chart.js/auto';
+
 
 @Component({
   selector: 'app-details-page',
   templateUrl: './details-page.page.html',
   styleUrls: ['./details-page.page.scss'],
 })
-export class DetailsPagePage implements OnInit {
+export class DetailsPagePage {
+  @ViewChild('chart') doughnutChart: ElementRef;
 
-  constructor(private router: Router) { }
 
-  ngOnInit() {
+  amountRemaining = 2500;
+  amountPaid = 1000;
+  totalAmount = 3500
+  merchantName = "Photgraphy";
+  chart: any;
+
+  private data = {
+    datasets: [{
+      label: 'Amount Paid',
+      data: [this.amountPaid, this.totalAmount - this.amountPaid],
+      backgroundColor: [
+        'rgb(0, 255, 0)',
+        'rgb(255, 0, 0)',
+      ],
+      hoverOffset: 4
+    }]
+  };
+
+  constructor(private router: Router) {
+    
+   }
+
+  ngAfterViewInit() {
+    // this.chart = new Chart(this.doughnutChart.nativeElement, {
+    //   type: 'doughnut',
+    //   data: this.data,
+    //   options: {
+    //     rotation: -90,
+    //     circumference: 180,
+    //   }
+    // })
   }
 
   back() {
     this.router.navigate(['/']);
   }
+  
 
 }
